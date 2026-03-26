36d6078008b854c1be09c97c779f4af0b0e86bc7
Hades <hades.otero@outlook.es>
2026-03-26 18:06:16 +0000

    0.4

--- javascript/index.js
+++ javascript/index.js
@@ -90,36 +90,42 @@ function steakInstruction(food,step) {
 steakInstruction("steak", 0)
   .then((step0) => {
     document.querySelector("#steak").innerHTML += `<li>${step0}</li>`;
-    return steakInstruction("steak", 1);
+    return steakInstruction("steak", 0);
   })
   .then((step1) => {
     document.querySelector("#steak").innerHTML += `<li>${step1}</li>`;
-    return steakInstruction("steak", 2);
+    return steakInstruction("steak", 1);
   })
   .then((step2) => {
     document.querySelector("#steak").innerHTML += `<li>${step2}</li>`;
-    return steakInstruction("steak", 3);
+    return steakInstruction("steak", 2);
   })
   .then((step3) => {
     document.querySelector("#steak").innerHTML += `<li>${step3}</li>`;
-    return steakInstruction("steak", 4);
+    return steakInstruction("steak", 3);
   })
   .then((step4) => {
     document.querySelector("#steak").innerHTML += `<li>${step4}</li>`;
-    return steakInstruction("steak", 5);
+    return steakInstruction("steak", 4);
   })
   .then((step5) => {
     document.querySelector("#steak").innerHTML += `<li>${step5}</li>`;
-    return steakInstruction("steak", 6);
+    return steakInstruction("steak", 5);
   })
   .then((step6) => {
     document.querySelector("#steak").innerHTML += `<li>${step6}</li>`;
-    document.querySelector("#steakImg").removeAttribute("hidden");
-    return steakInstruction("steak", 7);
+    
+    return steakInstruction("steak", 6);
   })
   .then((step7) => {
     document.querySelector("#steak").innerHTML += `<li>${step7}</li>`;
-    return steakInstruction("steak", 8);
+    return steakInstruction("steak", 7);
+  })
+  .then((step8) => {
+    document.querySelector("#steak").innerHTML += `<li>${step8}</li>`;
+    document.querySelector("#steakImg").removeAttribute("hidden");
+    return steakInstruction("steak", 7);
+    
   })
   .catch((error) => {
     console.log("The steak recipe had a problem: ", error);
@@ -146,18 +152,22 @@ broccoliInstruction();
 
 async function brusselsSproutsInstruction() {
   try {
-    for(j=0; j<8; j++) //async for uses the same variable
+    const promisesArray = [];
+    for(let j=0; j<8; j++) //async for uses the same variable
     {
-    const step = await obtainInstruction("brusselsSprouts", i);
+    promisesArray.push(obtainInstruction("brusselsSprouts", j));
+  }
+  const allSteps = await Promise.all(promisesArray);
+
+  allSteps.forEach((step)=> {
     document.querySelector("#brusselsSprouts").innerHTML += `<li>${step}</li>`;
-    }
-    
-    
+  })
     document.querySelector("#brusselsSproutsImg").removeAttribute("hidden");
   } catch (error) {
     console.log("The brussels sprouts recipe had a problem: ", error);
   }
 }
-brusselsSproutsInstruction();
+
+ brusselsSproutsInstruction();
 // Bonus 2 - Promise all 
 // ...
